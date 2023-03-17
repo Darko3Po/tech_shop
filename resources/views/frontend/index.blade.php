7589199b3496a782a9546781fc9a3642a07e08b0
Darko3Po <potajacdarko9@gmail.com>
2023-03-17 11:25:24 +0100

    Add home page and navbar

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.front')
+
+@section('title')
+	Welcome to Tech shop
+@endsection
+
+@section('content')
+	@include('layouts.inc.slider')
+	<h1>Welcome</h1>
+@endsection
+
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -7,38 +7,28 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>
+        @yield('title')
+    </title>
 
        <!--     Fonts and icons     -->
       <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
      
 
-       <link href="{{ asset('admin/css/custom.css') }}" rel="stylesheet">
-       <link href="{{ asset('admin/css/material-dashboard.css') }}" rel="stylesheet">
+       <link href="{{ asset('frontend/css/custom.css') }}" rel="stylesheet">
+       <link href="{{ asset('frontend/css/bootstrap5.css') }}" rel="stylesheet">
        
 </head>
 <body>
-    
-    <div class="wrapper">
-        @include('layouts.inc.sidebar')
-
-        <div class="main-panel">
-            @include('layouts.inc.adminnav')
-
-              <div class="content">
-                 @yield('content')
-
-              </div>
-              @include('layouts.inc.adminfooter')
-        </div>
+    @include('layouts.inc.frontnavbar')
+    <div class="content">
+        @yield('content')
     </div>
 
 
     <!-- Core JS File -->
-    <script src="{{ asset('admin/js/jquery.min.js') }}" defer></script>
-    <script src="{{ asset('admin/js/popper.min.js') }}" defer></script>
-    <script src="{{ asset('admin/js/bootstrap-material-design.min.js') }}" defer></script>
+    <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
    
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     @if (session('status'))
